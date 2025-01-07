16aaf3095b508a942b43ba920f2f959fd1c44f8d
indusara <deviruvanindusara@gmail.com>
2025-01-07 14:43:14 +0530

    splash,signin screen converted to a responsive design

--- app/src/main/java/com/example/tastewaveapp/SignInActivity.java
+++ app/src/main/java/com/example/tastewaveapp/SignInActivity.java
@@ -1,6 +1,8 @@
 package com.example.tastewaveapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signin);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //attach button to a button object
+        Button btn_SignIn = findViewById(R.id.btn_SignIn);
+
+        btn_SignIn.setOnClickListener(view -> {
+            //when click on skip button go to the home screen
+            Intent intent = new Intent(SignInActivity.this,HomeActivity.class);
+            startActivity(intent);
+            finish();//prevent going back to the login screen
         });
     }
 }
--- app/src/main/java/com/example/tastewaveapp/StartActivity.java
+++ app/src/main/java/com/example/tastewaveapp/StartActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,6 +27,23 @@ public class StartActivity extends AppCompatActivity {
 
         });
 
+        //attach button to a button object
+        Button btnSkip = findViewById(R.id.btn_skip);
+        Button btnContinue = findViewById(R.id.btn_continue);
 
+        btnSkip.setOnClickListener(view -> {
+            //when click on skip button go to the home screen
+            Intent intent = new Intent(StartActivity.this,HomeActivity.class);
+            startActivity(intent);
+            finish();//prevent going back to the login screen
+        });
+
+        btnContinue.setOnClickListener(view -> {
+            //when click on continue button go to the login screen
+            Intent intent = new Intent(StartActivity.this,SignInActivity.class);
+            startActivity(intent);
+            finish();//prevent going back to the login screen
+
+        });
     }
 }
